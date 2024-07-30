387f9127a01a0986e4d85883989e95fab83dd4d8
Anit Paul <127428414+Anit-Paul@users.noreply.github.com>
2024-07-30 20:27:39 +0530

    Update album.py

--- album.py
+++ album.py
@@ -18,12 +18,18 @@ text_widget.grid(row=0, column=0, columnspan=3, padx=30, pady=30)
 # Create a Label widget to display images
 img_label = Label(root)
 img_label.grid(row=0, column=0, columnspan=3, padx=60, pady=60)
+
 i=[0]
 
+status=Label(root,text=f"Image {i[0]+1} of Images {len(items)}\n")
+
+def update(index):
+    status.config(text=f"Image {index+1} of Images {len(items)}\n",font=('Helvetica', 12))
 
 def show_img(index):
     global img_label  # Using global variable for image label
     if 0 <= index < len(items):
+        update(index)
         img = ImageTk.PhotoImage(Image.open(items[index]).resize((600, 450), Image.LANCZOS))
         #Image.LANCZOS is a resampling filter used by the Pillow library for resizing images. It’s known for providing high-quality results when reducing the size of images
         img_label.config(image=img)
@@ -47,10 +53,10 @@ button_backward = Button(root, text='<<', padx=20, pady=10, command=lambda: bsho
 button_quit = Button(root, text='Exit', padx=20, pady=10, command=root.quit)
 
 # Place buttons in the grid
-button_backward.grid(row=1, column=0)
-button_quit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
-
+button_backward.grid(row=2, column=0)
+button_quit.grid(row=2, column=1)
+button_forward.grid(row=2, column=2)
+status.grid(row=1,column=1)
 # Display the first image if available
 if items:
     show_img(0)
